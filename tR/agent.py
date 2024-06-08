af5e1fa0e6416dc5882621bb8ae0b3ec2fd44568
unknown <yrezehi@gmail.com>
2024-06-08 17:41:06 +0300

    setup agent

---
+++ tR/agent.py
@@ -0,0 +1,16 @@
+from llama_index.core.tools import FunctionTool
+from llama_index.llms.ollama import Ollama
+from llama_index.core.agent import ReActAgent
+
+from functions import multiply
+
+functions_defs = [
+    multiply
+]
+
+tools = map(FunctionTool.from_defaults, functions_defs)
+
+llm = Ollama(model="qwen:0.5b", request_timeout=120.0)
+
+# initialize ReAct agent
+agent = ReActAgent.from_tools(tools, llm=llm, verbose=True)
---
+++ tR/functions/mathematical.py
@@ -0,0 +1,4 @@
+
+def multiply(a: int, b: int) -> int:
+    """Multiple two integers and returns the result integer"""
+    return a * b
